4716452c7c61259c55a45e4cc3a16ca9086d784e
Tomás <146328829+TomasSilvaGomes@users.noreply.github.com>
2024-04-27 19:20:53 +0100

    Alterações na classe, na sua funcao representação

---
+++ Trabalho_pratico/Analise.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+
+class Competition:
+
+    def __init__(self):
+        content = pd.read_csv('Cars_input.csv')
+        self.CAR_ID = content['CAR_ID']
+        self.Tire_Wear = content['Tire_Wear']
+        self.Fuel_Level = content['Fuel_Level']
+        self.Race_Position = content['Race_Position']
+        self.Laps_Since_Last_Pit = content['Laps_Since_Last_Pit']
+
+    # function that calculates the priority of the object
+    def priority(self):
+        weights = (0.5, 0.5, 0.1)
+        return (self.Tire_Wear * weights[0]) + (self.Fuel_Level * weights[1]) + (self.Race_Position * weights[2])
+
+    def __repr__(self):
+        # Creation of a list of tuples containing the car ID and priority
+        print(self.CAR_ID)
+        car_id = self.CAR_ID
+        priority = self.priority()
+        car_priorities = list(zip(car_id, priority))
+
+        # Sort the list by priority in descending order
+        car_priorities.sort(key=lambda y: y[1], reverse=True)
+
+        # Format the output as a string
+        output = ""
+        for car_id, priority in car_priorities:
+            output += f"Car ID: {car_id}, Priority: {priority:.2f}\n"
+        return output
+
+    def insert_car(self, car_id, tire_wear, fuel_level, race_position, laps_since_last_pit):
+        # method to insert data if the user wants to
+        self.CAR_ID
+        self.Tire_Wear
+        self.Fuel_Level
+        self.Race_Position
+        self.Laps_Since_Last_Pit
+
+
+cp = Competition()
+cp.insert_car(100, 0.2, 50, 1, 2)
+print(cp.__repr__())
---
+++ Trabalho_pratico/__main__.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    print("""
+Our task is to develop an algorithm that schedules pit stops for a team during a race. 
+The algorithm must decide the optimal time for each car to pit based on various factors
+such as tire wear, fuel levels, and the current race position.
+     """)
