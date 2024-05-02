f9255a95c49ff472882079d12952ca945e3516a9
Tomás <146328829+TomasSilvaGomes@users.noreply.github.com>
2024-05-02 18:20:05 +0100

    Finalização do projeto
    trocar if else para match case pois é menos custosa

--- Trabalho_pratico/Analise.py
+++ Trabalho_pratico/Analise.py
@@ -9,22 +9,38 @@ class Competition:
         self.csv_list = []
         self.priority_list = []
         with open('Cars_input.csv', 'r') as file:
-            next(file)
+            next(file)  # to start reading from the second line
             csv_reader = csv.reader(file)
             self.csv_list = list(csv_reader)
 
-    # function that calculates the priority of each car
+    # function that calculates the priority of each car including the accidents
     def priority(self):
         for row in self.csv_list:
-            if row[5].startswith('FULL'):
-                # the cars that had a full accident will be considered as priority 3 because the higher priority is
-                # 2.55 and full accident needs to have the highest of them all
-                calculator = (row[0], 3)
+            match row[5]:
+                case 'FULL_ACCIDENT':
+                    # if the car had a full accident the priority is set to a high value
+                    calculator1 = (row[0], 1000)
+                    self.priority_list.append(calculator1)
 
-            else:
-                calculator = (row[0], (float(row[1]) * self.w1) + ((1 - (int(row[2]) * 0.01)) * self.w2) + (
-                        self.w3 * ((len(self.csv_list) + 1) - int(row[3]))))
-            self.priority_list.append(calculator)
+                case 'PARTIAL_ACCIDENT':
+                    # the cars who had a partial or low accidents will set to a similar formula of the default priority,
+                    # the only difference is the fuel loss and tire loss
+                    calculator2 = (row[0], (((float(row[7]) * 0.01) + float(row[1])) * self.w1) + (
+                            (float(row[6]) + (1 - (int(row[2]) * 0.01))) * self.w2) + (
+                                           self.w3 * ((len(self.csv_list) + 1) - int(row[3]))))
+                    self.priority_list.append(calculator2)
+
+                case 'LOW_ACCIDENT':
+                    calculator3 = (row[0], (((float(row[7]) * 0.01) + float(row[1])) * self.w1) + (
+                            (float(row[6]) + (1 - (int(row[2]) * 0.01))) * self.w2) + (
+                                           self.w3 * ((len(self.csv_list) + 1) - int(row[3]))))
+                    self.priority_list.append(calculator3)
+
+                case _:
+                    # default formula of the priority
+                    calculator3 = (row[0], (float(row[1]) * self.w1) + ((1 - (int(row[2]) * 0.01)) * self.w2) + (
+                            self.w3 * ((len(self.csv_list) + 1) - int(row[3]))))
+                    self.priority_list.append(calculator3)
 
     def __repr__(self):
         # Sort the list by priority in descending order, based on the priority (second element)
@@ -32,6 +48,7 @@ class Competition:
         self.priority_list.sort(key=lambda x: x[1], reverse=True)
 
         # Format the output as a string
+        # the priority scale is not defined in certain scale
         output = ""
         for car_id, priority in self.priority_list:
             output += f"Car ID: {car_id}, Priority: {priority:.2f}\n"
